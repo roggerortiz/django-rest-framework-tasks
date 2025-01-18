f2971b9bb5f2588a0dc0e9c222fbadfa40d3621b
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2025-01-18 17:30:09 -0500

    rename done to important

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
 # Apply any outstanding database migrations
+python3 manage.py makemigrations
 python3 manage.py migrate
