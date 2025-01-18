efa211aa216c3be1d37fa829d7ee5209717a3ff6
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2025-01-18 17:10:49 -0500

    vercel deploy

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install requirements
+python3 -m pip install -r requirements.txt
+
+# Convert static asset files
+python3 manage.py collectstatic --noinput
+
+# Apply any outstanding database migrations
+python3 manage.py migrate
